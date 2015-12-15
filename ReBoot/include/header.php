9c6d0e4ba667545c206a59cd0bb3c7e2f4f576a6
mcdonald12 <mama.tata@wp.pl>
2015-12-15 17:39:20 +0100

    gh
    gh

---
+++ ReBoot/include/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!--[if lt IE 9]>
+<html class="lt-ie9" lang="en">
+<![endif]-->
+
+
+<!--[if !IE]>
+<html class="lt-ie9" lang="en"><!-->
+<html lang="pl">
+<!-- <![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<title>Marcin Jendrasik strona</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Szablon strony">
+		<meta name="author" content="Marcin Jendrasik">
+
+		<!-- Le styles -->
+		<link href="css/style.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<![endif]-->
+	 </head>
+	 <body>
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<div class="navbar">
+					  <div class="navbar-inner">
+						<div class="container">
+						 
+							  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+							  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							  </a>
+						 
+							  <!-- Be sure to leave the brand out there if you want it shown -->
+							  <a class="brand" href="#">ReBoot</a>
+						 
+							  <!-- Everything you want hidden at 940px or less, place within here -->
+							  <div class="nav-collapse collapse">
+								<!-- .nav, .navbar-search, .navbar-form, etc -->
+								<ul class="nav">
+								  <li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+									  Account
+									  <b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu pull-right">
+									  <li><a href="#">sample link</a></li>
+									  <li><a href="#">sample link</a></li>
+									  <li><a href="#">sample link</a></li>
+									</ul>
+								  </li>
+								</ul><!-- koniec drop down -->
+							  </div>
+					 
+						</div><!-- koniec container navbar -->
+					  </div>
+					</div>
+				</div>
+			</div><!--koniec row navbar  -->
+		</div>
